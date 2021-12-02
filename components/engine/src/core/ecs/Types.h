2b42feecfe0862cb321b18a18400d1bd491efd2a
Jakub Bednarek <260387@student.pwr.edu.pl>
2021-12-02 15:45:46 +0100

    Added Types class for defining types used by ECS system.

---
+++ components/engine/src/core/ecs/Types.h
@@ -0,0 +1,19 @@
+//
+// Created by BlackFlage on 02.12.2021.
+//
+
+#ifndef MAIN_TYPES_H
+#define MAIN_TYPES_H
+
+#include <bitset>
+#include <cstdint>
+
+using Entity = std::uint16_t;
+using Component = std::uint8_t;
+
+#define MAX_ENTITIES 1024
+#define MAX_COMPONENTS 128
+
+using Signature = std::bitset<MAX_COMPONENTS>;
+
+#endif //MAIN_TYPES_H
