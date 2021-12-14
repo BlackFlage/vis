ef7f78bfd6367ce551e3e66c28a8ccb29a352bad
Jakub Bednarek <260387@student.pwr.edu.pl>
2021-12-14 21:00:32 +0100

    Changed Input class for better mouse events handling.

--- components/engine/src/core/Input.cpp
+++ components/engine/src/core/Input.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Input.h"
+#include "Logger.h"
 
 namespace vis
 {
@@ -10,8 +11,19 @@ namespace vis
     {
         m_mouse_pos_x = 0.0f;
         m_mouse_pos_y = 0.0f;
-        m_mouse_delta_x = 0.0f;
-        m_mouse_delta_y = 0.0f;
+        m_last_mouse_pos_x = 0.0f;
+        m_last_mouse_pos_y = 0.0f;
+
+        m_mouse_buttons_state[0] = false;
+        m_mouse_buttons_state[1] = false;
+    }
+
+    Input::Input(float x_pos, float y_pos)
+    {
+        m_mouse_pos_x = x_pos;
+        m_mouse_pos_y = y_pos;
+        m_last_mouse_pos_x = x_pos;
+        m_last_mouse_pos_y = y_pos;
     }
 
     std::pair<float, float> Input::get_mouse_pos() const
@@ -21,29 +33,50 @@ namespace vis
 
     float Input::get_mouse_pos_x() const
     {
-        return m_mouse_pos_y;
+        return m_mouse_pos_x;
     }
 
     float Input::get_mouse_pos_y() const
     {
-        return m_mouse_pos_x;
+        return m_mouse_pos_y;
     }
 
-    void Input::add_mouse_pos(float a_pos_x, float a_pos_y)
+    void Input::set_mouse_pos(float a_pos_x, float a_pos_y)
     {
-        m_mouse_pos_x += a_pos_x;
-        m_mouse_pos_y += a_pos_y;
-        m_mouse_delta_x = a_pos_x;
-        m_mouse_delta_y = a_pos_y;
+        m_last_mouse_pos_x = m_mouse_pos_x;
+        m_last_mouse_pos_y = m_mouse_pos_y;
+        m_mouse_pos_x = a_pos_x;
+        m_mouse_pos_y = a_pos_y;
     }
 
     float Input::get_mouse_delta_x() const
     {
-        return m_mouse_delta_y;
+        return m_mouse_pos_x - m_last_mouse_pos_x;
     }
 
     float Input::get_mouse_delta_y() const
     {
-        return m_mouse_delta_y;
+        return m_mouse_pos_y - m_last_mouse_pos_y;
+    }
+
+    bool Input::is_mouse_button_clicked(int a_button) const
+    {
+        if(a_button < 0 || a_button > 1)
+        {
+            LOG_ERROR("Invalid argument, button must be between 0 and 1!");
+            return false;
+        }
+
+        return m_mouse_buttons_state[a_button];
+    }
+
+    void Input::set_button_state(int a_button, bool a_state)
+    {
+        if(a_button < 0 || a_button > 1)
+        {
+            LOG_ERROR("Invalid argument, button must be between 0 and 1!");
+        }
+
+        m_mouse_buttons_state[a_button] = a_state;
     }
 }
--- components/engine/src/core/Input.h
+++ components/engine/src/core/Input.h
@@ -14,20 +14,28 @@ namespace vis
     {
     public:
         Input();
+        Input(float x_pos, float y_pos);
+        ~Input() = default;
 
         float get_mouse_pos_x() const;
         float get_mouse_pos_y() const;
         float get_mouse_delta_x() const;
         float get_mouse_delta_y() const;
 
+        bool is_mouse_button_clicked(int a_button) const;
+
         std::pair<float, float> get_mouse_pos() const;
 
-        void add_mouse_pos(float a_pos_x, float a_pos_y);
+        void set_mouse_pos(float a_pos_x, float a_pos_y);
+        void set_button_state(int a_button, bool a_state);
     private:
         float m_mouse_pos_x;
         float m_mouse_pos_y;
-        float m_mouse_delta_x;
-        float m_mouse_delta_y;
+
+        float m_last_mouse_pos_x;
+        float m_last_mouse_pos_y;
+
+        bool m_mouse_buttons_state[2];
     };
 }
 
