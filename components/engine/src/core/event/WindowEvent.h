331fcdda891d21c368af6cf7b04fc5b34deb5e57
Jakub Bednarek <260387@student.pwr.edu.pl>
2021-12-14 20:59:50 +0100

    Added WindowFocusEvent.

--- components/engine/src/core/event/WindowEvent.h
+++ components/engine/src/core/event/WindowEvent.h
@@ -17,7 +17,7 @@ namespace vis
         WindowResizeEvent(RECT a_new_client_rect)
         : m_client_width(a_new_client_rect.right), m_client_height(a_new_client_rect.bottom) {}
 
-        inline static EventType get_static_type() { return EventType::WINDOW_RESIZE_EVENT; };
+        inline static EventType get_static_type() { return EventType::WINDOW_RESIZE_EVENT; }
         inline std::string get_name() const override { return "Window resize event."; }
         inline EventType get_event_type() const override { return EventType::WINDOW_RESIZE_EVENT; }
 
@@ -26,6 +26,21 @@ namespace vis
     private:
         UINT m_client_width;
         UINT m_client_height;
+    };
+
+    class WindowFocusEvent : public Event
+    {
+    public:
+        WindowFocusEvent(bool a_is_focued)
+        : m_is_focued(a_is_focued) {}
+
+        inline static EventType get_static_type() { return EventType::WINDOW_FOCUS_EVENT; }
+        inline std::string get_name() const override { return "Window focus event."; }
+        inline EventType get_event_type() const override { return EventType::WINDOW_FOCUS_EVENT; }
+
+        inline bool is_focused() const { return m_is_focued; }
+    private:
+        bool m_is_focued;
     };
 }
 
