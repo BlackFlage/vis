97890c85c16cc5501b8b51d4d30280b1d49f9ad3
Jakub Bednarek <260387@student.pwr.edu.pl>
2022-01-01 03:42:57 +0100

    Reworked obj files loading, now supporting more cases.

--- components/engine/src/core/resource_loaders/MeshLoader.cpp
+++ components/engine/src/core/resource_loaders/MeshLoader.cpp
@@ -14,7 +14,6 @@
 
 namespace vis
 {
-
     MeshLoader::MeshLoader()
     {
         m_path_to_index = {};
@@ -32,148 +31,53 @@ namespace vis
         std::ifstream file(a_path);
         if(!file)
         {
-            LOG_ERROR("Failed to open file: {0} while loading .obj file!", a_path.c_str());
+            LOG_ERROR("Failed to open .obj file: {0}", a_path.c_str());
             return MAX_RESOURCES;
         }
 
         std::vector<glm::vec3> vertices;
-        std::vector<unsigned int> indices;
+        std::vector<glm::vec2> texture_coords;
         std::vector<glm::vec3> normals;
-        std::vector<glm::vec2> textureCoords;
-        std::vector<unsigned int> vertexNormalIndices;
-        std::vector<unsigned int> textureCoordsIndices;
-        std::string line;
-        int faces_on_line;
-        GLuint geometry_type = GL_TRIANGLES;
-        bool found_geometry = false;
-        float x, y, z;
-        while (std::getline(file, line))
+        std::vector<glm::vec3> faces;
+        std::string current_line;
+        while((std::getline(file, current_line)))
         {
-            std::istringstream iss;
-            std::string operationType = line.substr(0, 2);
+            OPERATION_TYPE operation = get_operation_type(current_line);
 
-            if(operationType == "v ")
+            switch(operation)
             {
-                const char* data = line.c_str();
-
-                sscanf_s(data, "v %f %f %f", &x, &y, &z);
-                vertices.emplace_back(x, y, z);
-            }
-            else if(operationType == "vn")
-            {
-                const char* data = line.c_str();
-
-                sscanf_s(data, "vn  %f %f %f", &x, &y, &z);
-                normals.emplace_back(x, y, z);
-            }
-            else if(operationType == "vt")
-            {
-                const char* data = line.c_str();
-
-                sscanf_s(data, "vt %f %f", &x, &y);
-                textureCoords.emplace_back(x, y);
-            }
-            else if(operationType == "f ")
-            {
-                std::string delimiter = "/";
-                std::string data = line;
-
-                size_t end_pos = data.find("\r");
-                if(end_pos != std::string::npos)
-                {
-                    data.erase(end_pos, 2);
-                }
-
-                size_t pos = 0;
-                faces_on_line = 0;
-                while(data.size() > 0)
-                {
-                    if(!found_geometry)
-                    {
-                        faces_on_line++;
-                    }
-
-                    while(data.at(0) == ' ' || data.at(0) == 'f')
-                    {
-                        data = data.erase(0, 1);
-                    }
-
-                    pos = data.find(' ');
-                    std::string face = data.substr(0, pos);
-
-                    if(pos == std::string::npos)
-                    {
-                        pos = data.size() - 1;
-                    }
-
-                    data.erase(0, pos + 1);
-
-                    for(int i = 0; i < 3; i++)
-                    {
-                        pos = face.find(delimiter);
-                        if(pos == 0)
-                        {
-                            face = face.erase(0, 1);
-                            continue;
-                        }
-                        else if(pos == std::string::npos)
-                        {
-                            pos = face.size();
-                        }
-
-                        unsigned int value = std::stoi(face.substr(0, pos)) - 1;
-                        face = face.erase(0, pos + delimiter.length());
-
-                        switch(i)
-                        {
-                            case 0:
-                                indices.push_back(value);
-                                break;
-                            case 1:
-                                textureCoordsIndices.push_back(value);
-                                break;
-                            case 2:
-                                vertexNormalIndices.push_back(value);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-
-                if(!found_geometry)
-                {
-                    found_geometry = true;
-
-                    if(faces_on_line > 3)
-                    {
-                        geometry_type = GL_QUADS;
-                    }
-                }
+                case OPERATION_TYPE::VERTEX:
+                    parse_line<glm::vec3>(OPERATION_TYPE::VERTEX, vertices, current_line);
+                    break;
+                case OPERATION_TYPE::TEXTURE:
+                    parse_line<glm::vec2>(OPERATION_TYPE::TEXTURE, texture_coords, current_line);
+                    break;
+                case OPERATION_TYPE::NORMAL:
+                    parse_line<glm::vec3>(OPERATION_TYPE::NORMAL, normals, current_line);
+                    break;
+                case OPERATION_TYPE::FACE:
+                    extract_face_data(faces, current_line);
+                    break;
+                default:
+                    break;
             }
         }
 
         file.close();
-        std::vector<Vertex> outVertices;
-        outVertices.reserve(indices.size());
 
-        for(int i = 0; i < indices.size(); i++)
-        {
-            outVertices.emplace_back(Vertex{ vertices.at(indices.at(i)), normals.at(vertexNormalIndices.at(i)), textureCoords.at(textureCoordsIndices.at(i)) });
-        }
+        std::vector<Vertex> indexed_vertices;
+        std::vector<unsigned int> indices;
+        assign_vertices_and_indices(indexed_vertices, indices, vertices, texture_coords, normals, faces);
 
-        LOG_INFO("Successfully loaded file: {0}.", a_path.c_str());
+        Mesh* mesh = new Mesh(indexed_vertices, indices, GL_TRIANGLES);
+        std::uint16_t mesh_id = m_mesh_array->add_resource(mesh);
+        std::string name = std::filesystem::path(a_path).stem().string();
 
-        Mesh* mesh = new Mesh(outVertices, indices, geometry_type);
+        m_path_to_index.insert({a_path, mesh_id});
+        m_name_to_index.insert({name, mesh_id});
+        m_index_to_name.insert({mesh_id, name});
 
-        std::string file_name = std::filesystem::path(a_path).filename().string();
-
-        std::uint16_t id = m_mesh_array->add_resource(mesh);
-        m_path_to_index.insert({a_path, id});
-        m_name_to_index.insert({file_name, id});
-        m_index_to_name.insert({id, file_name});
-
-        return id;
+        return mesh_id;
     }
 
     void MeshLoader::delete_mesh(const std::string& a_path)
@@ -259,6 +163,108 @@ namespace vis
             {
                 LOG_ERROR("Error loading files from folder: {0}", a_exception.what());
             }
+        }
+    }
+
+    MeshLoader::OPERATION_TYPE MeshLoader::get_operation_type(std::string &a_line)
+    {
+        if(!a_line.empty())
+        {
+            auto pos_it = a_line.find(' ');
+
+            if(pos_it == std::string::npos)
+                return OPERATION_TYPE::OTHER;
+
+            std::string prefix = a_line.substr(0, pos_it);
+            a_line.erase(0, pos_it + 1);
+
+            if(prefix == "v")
+                return OPERATION_TYPE::VERTEX;
+            else if(prefix == "vt")
+                return OPERATION_TYPE::TEXTURE;
+            else if(prefix == "vn")
+                return OPERATION_TYPE::NORMAL;
+            else if(prefix == "f")
+                return OPERATION_TYPE::FACE;
+            else
+                return OPERATION_TYPE::OTHER;
+        }
+        else
+        {
+            return OPERATION_TYPE::OTHER;
+        }
+    }
+
+    void MeshLoader::extract_face_data(std::vector<glm::vec3>& a_destination, std::string& a_line)
+    {
+        std::stringstream line_ss(a_line);
+
+        std::vector<int> indices;
+        std::string face;
+        while(line_ss >> face)
+        {
+            while(!face.empty())
+            {
+                try
+                {
+                    size_t pos = face.find_first_of('/');
+                    if(pos == std::string::npos)
+                        pos = face.size();
+
+                    if(pos == 0)
+                    {
+                        indices.push_back(0);
+                        face.erase(0, 1);
+                    }
+                    else
+                    {
+                        indices.push_back(std::stoi(face.substr(0, pos)));
+                        face.erase(0, pos + 1);
+                    }
+                }
+                catch(std::exception& e)
+                {
+                    indices.push_back(0);
+                }
+            }
+
+            while(indices.size() < 3)
+            {
+                indices.push_back(0);
+            }
+            a_destination.push_back(glm::vec3(indices.at(0) - 1, indices.at(1) - 1, indices.at(2) - 1));
+            indices.clear();
+        }
+    }
+
+    void MeshLoader::assign_vertices_and_indices(std::vector<Vertex>& a_vert_dest, std::vector<unsigned int>& a_ind_dest,
+                                                 std::vector<glm::vec3>& a_vertices, std::vector<glm::vec2>& a_texture_coords,
+                                                 std::vector<glm::vec3>& a_normals, std::vector<glm::vec3>& a_faces)
+    {
+        int texture_coord_ind;
+        int normal_ind;
+        std::vector<Vertex> out_vertices;
+        for(const auto& face : a_faces)
+        {
+            Vertex v;
+
+            v.m_position = a_vertices.at(face.x);
+
+            texture_coord_ind = face.y;
+            normal_ind = face.z;
+
+            if(texture_coord_ind == -1)
+                v.m_textureCoords = glm::vec2(0.0f, 0.0f);
+            else
+                v.m_textureCoords = a_texture_coords.at(texture_coord_ind);
+
+            if(normal_ind == -1)
+                v.m_normals = glm::vec3(0.0f, 0.0f, 0.0f);
+            else
+                v.m_normals = a_normals.at(normal_ind);
+
+            a_vert_dest.push_back(v);
+            a_ind_dest.push_back(face.x);
         }
     }
 }
--- components/engine/src/core/resource_loaders/MeshLoader.h
+++ components/engine/src/core/resource_loaders/MeshLoader.h
@@ -21,15 +21,74 @@ namespace vis
         MeshLoader();
         ~MeshLoader() = default;
 
-        std::uint16_t       load_mesh(const std::string& a_path);
-        std::string         get_name(std::uint16_t a_id);
-        int                 get_id(const std::string& a_name);
-        void                delete_mesh(const std::string& a_path);
-        void                load_meshes_in_folders(const std::filesystem::path& a_path);
-        Mesh*               get_mesh(std::uint16_t a_id);
+        std::uint16_t            load_mesh(const std::string& a_path);
+        std::string              get_name(std::uint16_t a_id);
+        int                      get_id(const std::string& a_name);
+        void                     delete_mesh(const std::string& a_path);
+        void                     load_meshes_in_folders(const std::filesystem::path& a_path);
+        Mesh*                    get_mesh(std::uint16_t a_id);
         std::vector<const char*> get_available_meshes();
     private:
+        enum class OPERATION_TYPE
+        {
+            VERTEX,
+            TEXTURE,
+            NORMAL,
+            FACE,
+            OTHER
+        };
+
         void load_default_meshes();
+
+        void extract_face_data(std::vector<glm::vec3>& a_destination, std::string& a_line);
+
+        void assign_vertices_and_indices(std::vector<Vertex>& a_vert_dest, std::vector<unsigned int>& a_ind_dest,
+                                         std::vector<glm::vec3>& a_vertices, std::vector<glm::vec2>& a_texture_coords,
+                                         std::vector<glm::vec3>& a_normals, std::vector<glm::vec3>& a_faces);
+
+        void push_to_vec(std::vector<glm::vec3>& a_destination, float v0, float v1, float v2)
+        {
+            a_destination.push_back(glm::vec3(v0, v1, v2));
+        }
+
+        void push_to_vec(std::vector<glm::vec2>& a_destination, float v0, float v1, float v2)
+        {
+            a_destination.push_back(glm::vec2(v0, v1));
+        }
+
+        template<typename T>
+        void parse_line(MeshLoader::OPERATION_TYPE a_type, std::vector<T> &a_destination, std::string &a_line)
+        {
+            std::stringstream ss(a_line);
+            std::vector<float> values;
+
+            float v;
+            while(ss >> v)
+            {
+                values.push_back(v);
+            }
+
+            switch(a_type)
+            {
+                case OPERATION_TYPE::VERTEX:
+                case OPERATION_TYPE::NORMAL:
+                    if(values.size() == 3)
+                    {
+                        push_to_vec(a_destination, values.at(0), values.at(1), values.at(2));
+                    }
+                    break;
+                case OPERATION_TYPE::TEXTURE:
+                    if(values.size() == 2 || values.size() == 3)
+                    {
+                        push_to_vec(a_destination, values.at(0), values.at(1), 0);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        OPERATION_TYPE get_operation_type(std::string& a_line);
     private:
         std::unordered_map<std::string, std::uint16_t> m_path_to_index;
         std::unordered_map<std::uint16_t, std::string> m_index_to_name;
